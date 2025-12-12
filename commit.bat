e1c2b6068ce6a9dec62890e2d8450157d0ce03bb
visyura <cc.visyura@gmail.com>
2025-12-12 20:10:30 +0100

    -dynamic menu index -merge graphic designs

--- commit.bat
+++ commit.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 for /f "tokens=1-3 delims=/" %%a in ('date /t') do (
     set DAY=%%a
@@ -6,8 +7,8 @@ for /f "tokens=1-3 delims=/" %%a in ('date /t') do (
     set YEAR=%%c
 )
 
-set YEAR=%YEAR:~-2%
-set TODAY=%DAY%%MONTH%%YEAR%
+set YEAR=!YEAR:~-2!
+set TODAY=!DAY!!MONTH!!YEAR!
 
 if exist commit_info.txt (
     set /p LAST_COMMIT=<commit_info.txt
@@ -15,25 +16,25 @@ if exist commit_info.txt (
     set LAST_COMMIT=00000000
 )
 
-set LAST_DATE=%LAST_COMMIT:~0,6%
-set /a LAST_NUM=%LAST_COMMIT:~6%
+set LAST_DATE=!LAST_COMMIT:~0,6!
+set /a LAST_NUM=!LAST_COMMIT:~6!
 
-if "%TODAY%"=="%LAST_DATE%" (
-    set /a NEW_NUM=%LAST_NUM%+1
+if "!TODAY!"=="!LAST_DATE!" (
+    set /a NEW_NUM=!LAST_NUM!+1
 ) else (
-    set NEW_NUM=01
+    set /a NEW_NUM=1
 )
 
-if %NEW_NUM% LSS 10 (
-    set NEW_NUM=0%NEW_NUM%
+if !NEW_NUM! LSS 10 (
+    set NEW_NUM=0!NEW_NUM!
 )
 
-set COMMIT_ID=%TODAY%%NEW_NUM%
+set COMMIT_ID=!TODAY!!NEW_NUM!
 
-echo %COMMIT_ID% > commit_info.txt
+echo !COMMIT_ID! > commit_info.txt
 
 git add .
-git commit -m "%COMMIT_ID%"
+git commit -m "!COMMIT_ID!"
 git push
 
 echo.
